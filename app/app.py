5a7264931ccbeb9177d50073d7affcb5b8ef6b0c
flyingV_zy <flyingV_zy@163.com>
2019-11-10 21:37:23 +0800

    [1010]

--- app/app.py
+++ app/app.py
@@ -16,64 +16,18 @@ mongodb = MongoClient('localhost', 27017)
 db = mongodb.flask_1009
 activity = Activity(db)
 
-app = Flask(__name__)
+app = Flask('mini')
 
 
-# @app.route('/')
-# def index():
-#     all_goods = goods.get_main_page_goods(db)
-#
-#     # return send_file(io.BytesIO(img), mimetype="image/png")
-#     return jsonify(all_goods)
-#     # return 'hello'
-#
-#
-# @app.route("/image/<image_id>", methods=['GET'])
-# def get_one_image(image_id):
-#     img = image.get_img(db, image_id)
-#     return send_file(io.BytesIO(img), mimetype="image/jpg")
-#
-#
-# @app.route("/goods/<goods_id>", methods=['GET'])
-# def get_goods_details(goods_id):
-#     goods_images = goods.get_goods_detail(db, goods_id)
-#     return jsonify(goods_images)
-#
-#
+@app.route("/test", methods=['GET'])
+def get_test():
+    return 'test success!'
 
-#
-#
-# @app.route("/cart/", methods=['GET', 'POST'])
-# def cart():
-#     if request.method == 'GET':
-#         openid = request.args['openid']
-#         cart = user.get_cart(db, openid)
-#         return jsonify(cart)
-#     elif request.method == 'POST':
-#         data = json.loads(str(request.data, 'utf-8'))
-#         openid = data['openid']
-#         addedGodds = data['addedGoods']
-#         if addedGodds:
-#             res = user.add_to_cart(db, openid, addedGodds)
-#         else:
-#             updatedCart = data['cart']
-#             res = user.update_cart(db, openid, updatedCart)
-#         return res
-#
-#
-# @app.route("/address/", methods=['GET', 'POST'])
-# def get_user_address():
-#     if request.method == 'GET':
-#         openid = request.args['openid']
-#         address_list = []
-#         user_info = db.users.find_one({'openid': openid})
-#         try:
-#             address_list = user_info['address_list']
-#         except Exception:
-#             print(Exception)
-#         return jsonify(address_list)
-#     elif request.method == 'POST':
-#         return jsonify({'success': True})
+
+@app.route("/", methods=['GET'])
+def get_index():
+    return 'welcome to my bike life!'
+
 
 @app.route("/test", methods=['GET'])
 def get_test():
@@ -98,6 +52,8 @@ def get_main_page_activity():
     all_activity = []
     if request.method == 'GET':
         openid = request.args['openid']
+        if len(openid) < 30:
+            return jsonify([])
         all_activity = activity.getMainPageActivity(openid)
     return jsonify(all_activity)
 
@@ -160,4 +116,4 @@ def update_location():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
